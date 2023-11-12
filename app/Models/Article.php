c11425af7bd09dce89708813599d474b074eccf2
wael kasouha <waelkas99@gmail.com>
2023-11-12 14:42:41 -0500

    make Article model & migration

---
+++ app/Models/Article.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Article extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'content',
+        'published',
+        'published_at',
+    ];
+
+    protected $casts = [
+        'published' => 'boolean',
+        'published_at' => 'datetime',
+    ];
+}
