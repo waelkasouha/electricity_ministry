5e474f041b5e59c0f3daea22fcb0f3468443b628
Mohannad-Ali <mohannad.ali.2.3.6.9@gmail.com>
2023-11-12 12:22:03 -0700

    make Landing filament-resource

---
+++ app/Filament/Resources/LandingResource/Pages/CreateLanding.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\LandingResource\Pages;
+
+use App\Filament\Resources\LandingResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateLanding extends CreateRecord
+{
+    protected static string $resource = LandingResource::class;
+}
---
+++ app/Filament/Resources/LandingResource/Pages/EditLanding.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\LandingResource\Pages;
+
+use App\Filament\Resources\LandingResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditLanding extends EditRecord
+{
+    protected static string $resource = LandingResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
