c83b2a44753113b4a30636d2e96925e01af5dc46
wael kasouha <waelkas99@gmail.com>
2023-11-12 14:47:15 -0500

    make Article model & migration

--- database/migrations/2023_11_12_193958_create_articles_table.php
+++ database/migrations/2023_11_12_193958_create_articles_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->longText('content');
+            $table->boolean('published')->default(false);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
